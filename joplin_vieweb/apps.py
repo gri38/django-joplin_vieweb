be6e38b56d591844c914d1f631b490a39ca97735
gri38 <onzeweb@zaclys.net>
2021-06-11 15:50:24 +0200

    remove backgroun joplin sync: I should not develop in main branch ;-)

--- joplin_vieweb/apps.py
+++ joplin_vieweb/apps.py
@@ -12,10 +12,10 @@ class JoplinviewebConfig(AppConfig):
         AppConfig.__init__(self, app_name, app_module)
         self.sync_task_launched = False
     
-    def ready(self):
-        if not self.sync_task_launched:
-            self.sync_task_launched = True
-            start_synchronize_joplin()
-            # if you're running your app, using the python manage.py runserver command on Django, your application will run twice: One time to validate your models, and the other one to run your app.
-            # You can change this passing the option --noreload to the runserver command
-            # => to test synchro, run with --noreload to avoid 2 // synchro.
+    # def ready(self):
+        # if not self.sync_task_launched:
+            # self.sync_task_launched = True
+            # start_synchronize_joplin()
+            # # if you're running your app, using the python manage.py runserver command on Django, your application will run twice: One time to validate your models, and the other one to run your app.
+            # # You can change this passing the option --noreload to the runserver command
+            # # => to test synchro, run with --noreload to avoid 2 // synchro.
