6fa0e2cab4c2fb11e0fb8134961b57a6e40ae208
Nicolas Guillot <nicolas.guillot@mycronic.com>
2021-07-23 14:32:29 +0200

    Add code and table icons in note editor.

--- joplin_vieweb/static/joplinvieweb/js/NoteEditor.js
+++ joplin_vieweb/static/joplinvieweb/js/NoteEditor.js
@@ -41,7 +41,8 @@ class NoteEditor extends EventEmitter {
             tabSize: 4,
             previewImagesInEditor: true,
             imagePathAbsolute: true,
-            imageMaxSize: 1024*1024*1024*8 // 1GB
+            imageMaxSize: 1024*1024*1024*8, // 1GB
+            showIcons: ["code", "table", ],
         });
 
         // attach to cancel and commit buttons.
