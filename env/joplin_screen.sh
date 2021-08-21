6b4f6b578151bbfbaeb1fe62994dcf93ac8c0f4a
Nicolas Guillot <nicolas.guillot@mycronic.com>
2021-08-21 17:20:41 +0200

    fix script autostart

--- env/joplin_screen.sh
+++ env/joplin_screen.sh
@@ -3,7 +3,7 @@
 su -c "screen -dmS joplin" pi
 su -c "screen -S joplin -X title joplin\_server" pi
 su -c "screen -S joplin -X stuff \"sqlite3 ~/.config/joplin/database.sqlite \\\"INSERT INTO settings ('key', 'value') VALUES('api.token', '1234567890987654321')\\\"^M\"" pi
-su -c "screen -S joplin -X stuff \"joplin server start\"" pi
+su -c "screen -S joplin -X stuff \"joplin server start^M\"" pi
 su -c "screen -S joplin -X screen -t bash" pi
 su -c "screen -S joplin -X screen -t daphne" pi
 su -c "screen -S joplin -X stuff \"cd^M\"" pi
