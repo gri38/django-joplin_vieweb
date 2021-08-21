a89d8c48ecd79dd78ce797e1400fcff545756b1e
Nicolas Guillot <nicolas.guillot@mycronic.com>
2021-08-21 17:16:57 +0200

    fix script autostart

--- env/joplin_screen.sh
+++ env/joplin_screen.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-su -c "screen -dmS joplin\_screen" pi
-su -c "screen -S joplin\_screen -X title joplin\_server" pi
-su -c "screen -S joplin\_screen -p 0 -X stuff \"sqlite3 ~/.config/joplin/database.sqlite \\\"INSERT INTO settings ('key', 'value') VALUES('api.token', '1234567890987654321')\\\"^M\"" pi
-su -c "screen -S sessionName -p 0 -X stuff \"joplin server start\"" pi
-su -c "screen -S joplin_screen -X screen -t bash" pi
-su -c "screen -S joplin_screen -X next" pi
-su -c "screen -S joplin_screen -X screen -t daphne" pi
-su -c "screen -S joplin_screen -X next" pi
-su -c "screen -S joplin\_screen -p 0 -X stuff \"cd^M\"" pi
-su -c "screen -S joplin\_screen -p 0 -X stuff \"cd .joplin-vieweb^M\"" pi
-su -c "screen -S joplin\_screen -p 0 -X stuff \". venv/bin/activate^M\"" pi
-su -c "screen -S joplin\_screen -p 0 -X stuff \"cd joplinweb/^M\"" pi
-su -c "screen -S joplin\_screen -p 0 -X stuff \"daphne -p 8001 joplinweb.asgi:application^M\"" pi
+su -c "screen -dmS joplin" pi
+su -c "screen -S joplin -X title joplin\_server" pi
+su -c "screen -S joplin -X stuff \"sqlite3 ~/.config/joplin/database.sqlite \\\"INSERT INTO settings ('key', 'value') VALUES('api.token', '1234567890987654321')\\\"^M\"" pi
+su -c "screen -S joplin -X stuff \"joplin server start\"" pi
+su -c "screen -S joplin -X screen -t bash" pi
+su -c "screen -S joplin -X screen -t daphne" pi
+su -c "screen -S joplin -X stuff \"cd^M\"" pi
+su -c "screen -S joplin -X stuff \"cd .joplin-vieweb^M\"" pi
+su -c "screen -S joplin -X stuff \". venv/bin/activate^M\"" pi
+su -c "screen -S joplin -X stuff \"cd joplinweb/^M\"" pi
+su -c "screen -S joplin -X stuff \"daphne -p 8001 joplinweb.asgi:application^M\"" pi
